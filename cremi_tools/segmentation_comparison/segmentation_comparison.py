9350a494e7dda16e1c058dde2173bad96d67bf93
Constantin Pape <c.pape@gmx.net>
2018-01-10 23:31:49 +0100

    Updated segmentation postprocessing

--- cremi_tools/segmentation_comparison/segmentation_comparison.py
+++ cremi_tools/segmentation_comparison/segmentation_comparison.py
@@ -12,14 +12,18 @@ class SegmentationComparison(object):
         self.uv_ids = self.rag.uvIds()
         self.volume_builder = nrag.ragCoordinates(self.rag, self.n_threads)
 
+    def active_edges(self, seg):
+        nodes = nrag.gridRagAccumulateLabels(self.rag, seg)
+        return nodes[self.uv_ids[:, 0]] != nodes[self.uv_ids[:, 1]]
+
+    def active_edge_volume(self, seg):
+        edges = self.active_edges(seg)
+        return self.volume_builder.edgesToVolume(edges,
+                                                 edgeDirection=0,
+                                                 numberOfThreads=self.n_threads)
+
     def edge_difference(self, seg_a, seg_b):
-        nodes_a = nrag.gridRagAccumulateLabels(self.rag, seg_a)
-        edges_a = nodes_a[self.uv_ids[:, 0]] != nodes_a[self.uv_ids[:, 1]]
-
-        nodes_b = nrag.gridRagAccumulateLabels(self.rag, seg_b)
-        edges_b = nodes_b[self.uv_ids[:, 0]] != nodes_b[self.uv_ids[:, 1]]
-
-        return edges_a != edges_b
+        return self.active_edges(seg_a) != self.active_edges(seg_b)
 
     def edge_difference_volume(self, seg_a, seg_b):
         edge_diff = self.edge_difference(seg_a, seg_b).astype('uint8')
--- examples/segmentation_comparison/knott_segmentations.py
+++ examples/segmentation_comparison/knott_segmentations.py
@@ -10,6 +10,8 @@ def compare_segs():
     seg_path = '/home/consti/Work/data_neuro/knott_data/testset/knott_test_seg.h5'
     mc0_path = '/home/consti/Work/data_neuro/knott_data/results/multicut_paper_cut_cc.h5'
     mc1_path = '/home/consti/Work/data_neuro/knott_data/results/multicut.h5'
+    nn_path = '/home/consti/Work/data_neuro/knott_data/testset/pmaps/knott_test_pmap_icv1.h5'
+    rf_path = '/home/consti/Work/data_neuro/knott_data/testset/pmaps/knott_test_pmap_rf.h5'
 
     seg = vigra.readHDF5(seg_path, 'data').astype('uint32', copy=False)
     mc0 = vigra.readHDF5(mc0_path, 'data').astype('uint32', copy=False)
@@ -19,8 +21,10 @@ def compare_segs():
     edge_vol = seg_comparison.edge_difference_volume(mc0, mc1)
 
     raw = vigra.readHDF5(raw_path, 'data')
-    volumina.view([raw, seg, mc0, mc1, edge_vol],
-                  ['raw', 'seg', 'mc-niko', 'mc', 'edge-diff'])
+    nn = vigra.readHDF5(nn_path, 'data')
+    rf = vigra.readHDF5(rf_path, 'data')
+    volumina.view([raw, rf, nn, seg, mc0, mc1, edge_vol],
+                  ['raw', 'rf', 'nn', 'seg', 'mc-niko', 'mc', 'edge-diff'])
 
 
 if __name__ == '__main__':
