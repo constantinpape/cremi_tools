5e167110d01a34ffb754559a440fd825977a5d17
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-01-19 23:15:53 -0500

    Some viewer fixes

--- cremi_tools/viewer/bigcat/bigcat.py
+++ cremi_tools/viewer/bigcat/bigcat.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 
+# TODO ingest paths and keys
 def view(ram_limit=16):
     file_path = os.path.split(os.path.realpath(__file__))[0]
     jar = os.path.join(file_path, 'bigcat-0.0.3-SNAPSHOT-jar-with-dependencies.jar')
