06ccff8a05f8e06b480616a29dfcd2f5a7ecb5dd
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-02-24 17:15:33 -0500

    Finish mutex ws class (not tested)

--- cremi_tools/segmentation/watershed.py
+++ cremi_tools/segmentation/watershed.py
@@ -1,6 +1,7 @@
 import multiprocessing
 # from concurrent import futures
 import numpy as np
+import numpy.masked_array as ma
 import vigra
 from scipy.ndimage.morphology import distance_transform_edt
 
@@ -263,6 +264,25 @@ class MutexWatershed(Oversegmenter):
         self.invert_repulsive_channels = invert_repulsive_channels
         self.randomize_bounds = randomize_bounds
 
+    # TODO randomize bounds ?!
+    def _sort_edges(self, input_):
+        mask = np.ones(input_.shape)
+        # use all attractive edges
+        mask[:self.seperating_channel] = 0
+        # subsample repulsive edges
+        if len(self.strides) == 3:
+            mask[self.seperating_channel:,
+                 ::self.strides[0],
+                 ::self.strides[1],
+                 ::self.strides[2]] = 1
+        else:
+            raise NotImplementedError()
+        masked_edges = ma.array(input_, mask=mask, fill_value=999)
+        # TODO: compute n_used_edges analytically from strides
+        n_used_edges = mask.size - mask.sum()
+        # axis=None sorts the flattend array
+        return masked_edges.argsort(axis=None)[:n_used_edges]
+
     # TODO subsample before sorting ?!
     def _oversegmentation_impl(self, input_):
         assert input_.ndim == 4
@@ -271,17 +291,20 @@ class MutexWatershed(Oversegmenter):
             input_[self.seperating_channel:] *= -1
             input_[self.seperating_channel:] += 1
 
-        # TODO subsampling before sorting
-        sorted_edges = np.argsort(input_.ravel())
+        # TODO make sure the pre-sorting works
+        sorted_edges = self._sorted_edges(input_)
+        # sorted_edges = np.argsort(input_.ravel())
 
         # run the mst watershed
         mst = cmst.ConstrainedWatershed(np.array(input_.shape[1:]),
                                         self.offsets,
                                         self.seperating_channel,
-                                        self.strides)  # need [1, 1, 1] strides if we subsample before sorting
+                                        np.array([1, 1, 1]))
+        # self.strides)  # need [1, 1, 1] strides if we subsample before sorting
+
         # don't need this if we subsample before sorting
-        if self.randomize_bounds:
-            mst.compute_randomized_bounds()
+        # if self.randomize_bounds:
+        #     mst.compute_randomized_bounds()
         mst.repulsive_ucc_mst_cut(sorted_edges, 0)
         segmentation = mst.get_flat_label_image().reshape(input_.shape[1:])
         return segmentation
@@ -299,4 +322,20 @@ class MutexWatershed(Oversegmenter):
         # TODO is this correct ?
         input_[:3, mask] = 1
 
-        # TODO do the rest
+        # TODO make sure the pre-sorting works
+        sorted_edges = self._sorted_edges(input_)
+        # sorted_edges = np.argsort(input_.ravel())
+
+        # run the mst watershed
+        mst = cmst.ConstrainedWatershed(np.array(input_.shape[1:]),
+                                        self.offsets,
+                                        self.seperating_channel,
+                                        np.array([1, 1, 1]))
+        # self.strides)  # need [1, 1, 1] strides if we subsample before sorting
+
+        # don't need this if we subsample before sorting
+        # if self.randomize_bounds:
+        #     mst.compute_randomized_bounds()
+        mst.repulsive_ucc_mst_cut(sorted_edges, 0)
+        segmentation = mst.get_flat_label_image().reshape(input_.shape[1:])
+        return segmentation
