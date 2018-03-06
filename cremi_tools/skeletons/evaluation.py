5a8b82c81071c8892de4cb99c9ed2421fa49bd0a
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-03-06 18:58:33 -0500

    Serialization support for metrics

--- cremi_tools/skeletons/evaluation.py
+++ cremi_tools/skeletons/evaluation.py
@@ -2,7 +2,6 @@ import os
 import nifty.skeletons as nskel
 
 
-# TODO enable serialization of node_assignment
 def build_skeleton_metrics(label_file, skeleton_file, n_threads=-1):
     assert os.path.exists(label_file), label_file
     assert os.path.exists(skeleton_file), skeleton_file
@@ -10,3 +9,13 @@ def build_skeleton_metrics(label_file, skeleton_file, n_threads=-1):
     skeleton_ids = [int(sk) for sk in skeleton_ids if sk.isdigit()]
     skeleton_ids.sort()
     return nskel.SkeletonMetrics(label_file, skeleton_file, skeleton_ids, n_threads)
+
+
+def load_skeleton_metrics(label_file, skeleton_file, serialization_file):
+    assert os.path.exists(label_file), label_file
+    assert os.path.exists(skeleton_file), skeleton_file
+    assert os.path.exists(serialization_file), serialization_file
+    skeleton_ids = os.listdir(skeleton_file)
+    skeleton_ids = [int(sk) for sk in skeleton_ids if sk.isdigit()]
+    skeleton_ids.sort()
+    return nskel.SkeletonMetrics(label_file, skeleton_file, skeleton_ids, serialization_file)
