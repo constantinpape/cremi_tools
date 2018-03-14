2880ec85659fe16e317ddf0fcb63c8b8ad222b95
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-03-14 15:59:13 -0400

    Implemnt lmc segmenter

--- cremi_tools/segmentation/multicut.py
+++ cremi_tools/segmentation/multicut.py
@@ -1,6 +1,7 @@
 from .base import Segmenter
 
 import numpy as np
+import nifty
 import nifty.graph.opt.multicut as nmc
 import nifty.graph.opt.lifted_multicut as nlmc
 from nifty import Configuration
@@ -133,14 +134,13 @@ class Multicut(Segmenter):
         return node_labels
 
 
-# FIXME
-class LiftedMulticut(Segmenter):
+# TODO this does not fit into th `Segmenter` scheme that easily
+# for now, it doesn't inherit from it
+class LiftedMulticut(object):
     solvers = ["greedy-additive", "kernighan-lin", "fusion-moves"]
 
     def __init__(self, solver, beta=.5, weight_edges=True, **solver_options):
         assert solver in self.solvers
-        if solver == "ilp":
-            assert self._have_ilp()
         self.solver = solver
         assert 0. < beta < 1.
         self.beta = beta
@@ -154,29 +154,45 @@ class LiftedMulticut(Segmenter):
         else:
             return transform_probabilities_to_costs(probabilities, self.beta)
 
+    def _get_fusion_moves(self, objective):
+        seeding_strategy = self.solver_options.get('seeding-strategy', 'SEED_FROM_LOCAL')
+        sigma = self.solver_options.get('sigma', 10.)
+        seed_fraction = self.solver_options.get('seed_fraction', .1)
+        pgen = objective.watershedProposalGenerator(seedingStrategy=seeding_strategy,
+                                                    sigma=sigma,
+                                                    numberOfSeeds=seed_fraction)
+        # we leave the number of iterations at default values for now
+        return objective.fusionMoveBasedFactory(proposalGenerator=pgen).create(objective)
+
     # TODO kwarg for verbosity
     # TODO support logging visitor
-    def _segmentation_impl(self, graph, costs, time_limit=None, **kwargs):
-        objective = nlmc.multicutObjective(graph, costs)
+    def __call__(self, local_uvs, lifted_uvs,
+                 local_costs, lifted_costs,
+                 time_limit=None, **kwargs):
+        n_nodes = int(local_uvs.max()) + 1
+        graph = nifty.graph.undirectedGraph(n_nodes)
+        graph.insertEdges(local_uvs)
+        objective = nlmc.liftedMulticutObjective(graph)
 
+        # TODO local vs. lifted weighting ?!
+
+        objective.setCosts(local_uvs, local_costs)
+        objective.setCosts(lifted_uvs, lifted_costs)
+
+        # TODO visitors and time limit!
+        # TODO would be nice to have solver chaining in nifty for lmc too
+        solver_ga = objective.greedyAdditiveFactory().create(objective)
+        # first solve greedy-agglomerative
+        node_labels = solver_ga.optimize()
         if self.solver == 'greedy-additive':
-            solver_impl = objective.greedyAdditiveFactory().create(objective)
-        elif self.solver == 'kernighan-lin':
-            warmstart = self.solver_options.get('warmstart_greedy', True)
-            # TODO need to set this if we use verbosity
-            # greedyVisitNth = kwargs.pop('greedyVisitNth', 100)
-            solver_impl = objective.kernighanLinFactory(warmStartGreedy=warmstart).create(objective)
-        elif self.solver == 'fusion-moves':
-            solver_impl = self._get_fusion_moves(objective)
-        elif self.solver == 'ilp':
-            ilp_backend = self.solver_options.get("ilp_backend", None)
-            solver_impl = objective.multicutIlpFactory(ilpSolver=ilp_backend).create(objective)
-
-        # TODO this needs to change once we suport verbosity / logging
-        if time_limit is None:
-            node_labels = solver_impl.optimize()
+            return node_labels
         else:
-            visitor = objective.verboseVisitor(visitNth=100000000, timeLimitSolver=time_limit)
-            node_labels = solver_impl.optimize(visitor=visitor)
+            solver_kl = objective.liftedMulticutKernighanLinFactory().create(objective)
+            node_labels = solver_kl.optimize(node_labels)
+            if self.solver == 'kernighan-lin':
+                return node_labels
+            elif self.solver == 'fusion-moves':
+                solver_fm = self._get_fusion_moves(objective)
+                node_labels = solver_fm.optimize(node_labels)
 
         return node_labels
