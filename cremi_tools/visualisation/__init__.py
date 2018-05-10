6d68bae2aa5088605a8b83b824fe4ccfca09d766
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-05-10 16:16:03 -0400

    Implement edge weight visualisation for subvolumes

--- cremi_tools/visualisation/__init__.py
+++ cremi_tools/visualisation/__init__.py
@@ -1,2 +1,2 @@
 from .segmentation_comparison import SegmentationComparison
-from .edge_visualisation import visualize_probabilities
+from .edge_visualisation import visualize_probabilities, visualize_probabilities_for_subvolume
